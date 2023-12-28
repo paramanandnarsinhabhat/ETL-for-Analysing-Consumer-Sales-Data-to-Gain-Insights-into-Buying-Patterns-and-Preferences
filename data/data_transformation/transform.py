c3e84c6ee5caa5f2eeeccd3afb4253faac93cadd
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2023-12-28 12:20:14 +0530

    Initial function for transformation written for Standardizing date formats     - Filling missing values     - Converting text to lowercase

--- data/data_transformation/transform.py
+++ data/data_transformation/transform.py
@@ -13,3 +13,17 @@ csv_file_path = '/Users/paramanandbhat/Downloads/SalesJan2009.csv'
 # Call the function to read the data
 sales_data = common_utils.read_csv_file(csv_file_path)
 
+# Step 1: Define a function for the transformation stage
+def transform_data(df):
+    """
+    Apply transformations to the dataframe such as:
+    - Standardizing date formats
+    - Filling missing values
+    - Converting text to lowercase
+    """
+    # Standardize date formats
+    df['Transaction_date'] = pd.to_datetime(df['Transaction_date'])
+    df['Account_Created'] = pd.to_datetime(df['Account_Created'])
+    df['Last_Login'] = pd.to_datetime(df['Last_Login'])
+    
+
