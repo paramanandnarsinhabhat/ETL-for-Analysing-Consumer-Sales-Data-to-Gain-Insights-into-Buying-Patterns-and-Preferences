9072fad60896063db67bccf008d186dbb0754d89
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2023-12-28 12:16:14 +0530

    changes to read file in transform.py

--- data/data_transformation/transform.py
+++ data/data_transformation/transform.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import sys
+import os
 
+# Add the project root to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+# Now you can import common_utils
+from utilities import common_utils
+
+# Define the path to the CSV file
+csv_file_path = '/Users/paramanandbhat/Downloads/SalesJan2009.csv'
 # Call the function to read the data
-sales_data = read_csv_file(csv_file_path)
+sales_data = common_utils.read_csv_file(csv_file_path)
+
