c2ffb73ac707210b25caefb15e31b65e8665c9e5
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2023-12-28 16:11:24 +0530

    extract has been added to etl pipeline

---
+++ etl_pipeline.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sys
+import os
+
+
+# Add the project root to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+# Now you can import common_utils
+from utilities import common_utils
+from data.data_extraction.extraction import extract
+from data.data_transformation.transform import transform
+from data.data_loading.load import load
+
+csv_file_path = '/Users/paramanandbhat/Downloads/SalesJan2009.csv'
+
+def run_etl():
+    # Extract
+    extracted_data = extract(csv_file_path)
+
+
+run_etl()
