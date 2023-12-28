357939664a94fe7c88caf4523da4db71504986c0
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2023-12-28 17:44:12 +0530

    Changes for transform in etl pipeline completed

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.
 # Now you can import common_utils
 from utilities import common_utils
 from data.data_extraction.extraction import extract
-from data.data_transformation.transform import transform
+from data.data_transformation.transform import transform_data
 from data.data_loading.load import load
 
 csv_file_path = '/Users/paramanandbhat/Downloads/SalesJan2009.csv'
@@ -17,6 +17,11 @@ csv_file_path = '/Users/paramanandbhat/Downloads/SalesJan2009.csv'
 def run_etl():
     # Extract
     extracted_data = extract(csv_file_path)
+
+# Transform
+    transformed_data = transform_data(extracted_data)
+
+    
 
 
 run_etl()
