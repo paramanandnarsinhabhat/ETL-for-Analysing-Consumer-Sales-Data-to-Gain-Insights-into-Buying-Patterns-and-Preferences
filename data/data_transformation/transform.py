7c65e599d684a6e5d0ec6a0b7cc1892f2da43568
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2023-12-28 12:36:17 +0530

    python transform.py test passes

--- data/data_transformation/transform.py
+++ data/data_transformation/transform.py
@@ -15,16 +15,12 @@ sales_data = common_utils.read_csv_file(csv_file_path)
 
 # Splitting the transform_data function into smaller, more specialized functions
 
-def standardize_date_formats(df, date_format=None):
+def standardize_date_formats(df, columns):
     """
     Standardize the date formats for given columns.
-    
-    :param df: DataFrame containing the date columns.
-    :param date_format: A string representing the format to parse the dates. Example: '%Y-%m-%d %H:%M:%S'
     """
-    date_columns = ['Transaction_date', 'Account_Created', 'Last_Login']
-    for col in date_columns:
-        df[col] = pd.to_datetime(df[col], format=date_format, errors='coerce')
+    for col in columns:
+        df[col] = pd.to_datetime(df[col])
     return df
 
 def fill_missing_values(df, fill_values):
@@ -51,16 +47,10 @@ def transform_data(df):
     - Filling missing values
     - Converting text to lowercase
     """
-    df = standardize_date_formats(sales_data, date_format='%Y-%m-%d %H:%M:%S')
+    df = standardize_date_formats(df, ['Transaction_date', 'Account_Created', 'Last_Login'])
     df = fill_missing_values(df, {'State': 'Unknown', 'US Zip': 0})
     df = convert_text_to_lowercase(df)
     return df
 
-# Now let's apply the transformation to the loaded data
-transformed_sales_data = transform_data(sales_data)
-
-# Display the first few rows of the DataFrame after transformation
-transformed_sales_data.head()
-
-
-
+df_transformed = transform_data(sales_data)
+print(df_transformed.head())
